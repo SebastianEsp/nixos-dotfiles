eaa57ff968cbd8ee3fc4d237c02f96a434b1eb9c
SebastianEsp <sebastianesp@hotmail.com>
2022-11-26 13:54:01 +0100

    zsh

--- home.nix
+++ home.nix
@@ -28,6 +28,7 @@
     firefox
     discord
     thunderbird
+    vscode
   ];
 
   # Git configuration
@@ -40,6 +41,31 @@
     };
   };
 
+  # zsh configuration
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      ll = "ls -l";
+      update = "sudo nixos-rebuild switch";
+    };
+    history = {
+      size = 10000;
+      path = "${config.xdg.dataHome}/zsh/history";
+    };
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.fetchFromGitHub {
+          owner = "romkatv";
+	  repo = "powerlevel10k";
+	  rev = "v1.16.1";
+	  sha256 = "0fkfh8j7rd8mkpgz6nsx4v7665d375266shl1aasdad8blgqmf0c";
+        };
+      }  
+    ];
+  };
+
   # Raw configuration files
   home.file.".vimrc".source = ./vimrc;
+  home.file.".zshrc".source = ./zshrc;
 }
